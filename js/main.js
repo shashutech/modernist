b5f289c841ae3202e719533bc3c6880382494f83
Kumar Shashwat <kumarshashwat62@gmail.com>
2018-11-18 16:50:44 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// $(window).scroll(function () {
+//     const scroll = $(window).scrollTop();
+//     if (scroll >= 60)
+//         $('nav').addClass('navbar-dark');
+//     else
+//     if ($('nav').hasClass('navbar-dark'))
+//         $('nav').removeClass('navbar-dark');
+// });
